0dd1e44b35cc772cc77f2a42d661a76bb2045b01
Imam-f <mohamadimamfirdaus17@gmail.com>
2024-12-15 21:10:19 +0700

    add cython file

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@ import numpy
 import sys
 import tkinter
 
+import example
+
 # cp -r ~/.local/share/uv/python/cpython-3.12.8-linux-x86_64-gnu/lib/tcl8.6 .venv/lib/
 
 def main():
@@ -11,6 +13,8 @@ def main():
     print("Python version:", sys.version)
     tcl = tkinter.Tcl()
     print(tcl.eval("puts \"Hello, world!\""))
+
+    print(example.primes(10))
 
 
 if __name__ == "__main__":
